c7482fb56e97f17c0ad2e44dc449699692d3da17
Yuichi Shiwaku <yuichi.shiwaku@gmail.com>
2015-01-07 19:14:08 +0900

    added config

--- pepe.go
+++ pepe.go
@@ -2,66 +2,15 @@ package main
 
 import (
 	"os"
-	"encoding/json"
-	"net/http"
-	"net/url"
 	"github.com/go-fsnotify/fsnotify"
 	"log"
 	"os/signal"
 	"github.com/shiwork/pepe/config"
+	"github.com/shiwork/slack/incoming"
 )
 
-var webhookUrl string = os.Getenv("PEPE_SLACK_INCOMING_URL")
-
-type field struct {
-	Title string `json:"title"`
-	Value string `json:"value"`
-	Short bool `json:"short"`
-}
-
-type attachment struct {
-	Fallback string `json:"fallback"`
-	Pretext  string `json:"pretext"`
-	Color    string `json:"color"`
-	Fields   []*field `json:"fields"`
-}
-
-type payload struct{
-	Attachments []*attachment `json:"attachments"`
-}
-
-func Post(title string, description string) error {
-	p, err := json.Marshal(&payload{
-		Attachments: []*attachment{
-			&attachment{
-				Fallback: title,
-				Pretext: title,
-				Fields: []*field{
-					&field{
-						Title: "",
-						Value: description,
-						Short: false,
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	_, err = http.PostForm(webhookUrl, url.Values{
-		"payload": []string{string(p)},
-	})
-
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func main() {
-	conf, err := config.Parse("/Users/yuichi.shiwaku/go/src/github.com/shiwork/pepe/config.json")
+	conf, err := config.Parse("./config.json")
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(0)
@@ -76,12 +25,33 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 
+	var slack_incoming_conf incoming.IncomingConf
+	slack_incoming_conf.WebHookUrl = conf.Slack.IncomingWebHook
+
 	go func() {
 		log.Println("start watch")
 		for {
 			select {
 			case event:= <-watcher.Events:
 				log.Println("event:", event)
+				// post Slack
+				var field incoming.Field
+				field.Short = false
+				field.Value = "テストだよー"
+				field.Title = ""
+
+				var attachment incoming.Attachment
+				attachment.Fallback = "fallbackだよ"
+				attachment.Pretext = event.Name
+				attachment.Fields = []*incoming.Field{&field}
+
+				var payload incoming.Payload
+				payload.Attachments = []*incoming.Attachment{&attachment}
+
+				err := incoming.Post(slack_incoming_conf, payload);
+				if err != nil {
+					log.Println("error:", err)
+				}
 			case err:= <-watcher.Errors:
 				log.Println("error:", err)
 			}
